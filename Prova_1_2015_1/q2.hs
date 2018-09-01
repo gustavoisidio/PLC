cb0815dd3d75c2d0cbb9c401b1335f13cd7d6c20
gustavoisidio <gisf@cin.ufpe.br>
2018-09-01 17:02:43 -0300

    Questão 2 - Função acertos

--- Prova_1_2015_1/q2.hs
+++ Prova_1_2015_1/q2.hs
@@ -10,5 +10,17 @@ premiados :: Resultado -> Jogos -> Int
 premiados r [] = 0 -- Caso base é quando testarmos todos os jogos
 premiados [] (j:js) = 0 -- Para o caso hipotético em que o resultado é vazio 
 premiados r (j:js) | r == j = (1 + (premiados r js))
-                   | otherwise = (premiados r js) 
+                   | otherwise = (premiados r js)
 
+-- Compara o resultado com um jogo e retorna quantos acertos aquele jogo teve
+mitnick :: Resultado -> [Int] -> Int
+mitnick _ [] = 0 -- Caso base é quando testarmos todos os jogos
+mitnick [] _ = 0 -- Para o caso hipotético em que o resultado é vazio
+mitnick (r:rs) (j) | elem r j = (1 + mitnick rs j) 
+                   | otherwise = (mitnick rs j)
+
+-- Chama comparação de cada jogo e retorna a lista com quantos acertos cada um dos jogos teve
+acertos :: Resultado -> Jogos -> [Int]
+acertos r [] = [] -- Caso base é quando testarmos todos os jogos
+acertos [] (j:js) = [] -- Para o caso hipotético em que o resultado é vazio
+acertos r (j:js) = (mitnick r j):(acertos r js)  
