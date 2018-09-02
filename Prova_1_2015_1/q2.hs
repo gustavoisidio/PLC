7270458f91a01a77ae279b0ea1c0fe2ee9e6dadb
gustavoisidio <gisf@cin.ufpe.br>
2018-09-01 21:32:08 -0300

    Questão 2 - Função numPremios

--- Prova_1_2015_1/q2.hs
+++ Prova_1_2015_1/q2.hs
@@ -2,7 +2,7 @@ type Resultado = [Int]
 type Jogos = [[Int]]
 
 -- Um exemplo de jogo com 2 premiados
-exJogo = [[12,13,14,15,16,17], [1, 2, 3, 4, 5, 6], [6,7,8,9,10,11], [12,13,14,15,16,17]]  :: Jogos
+exJogo = [[12,13,14,15,16,17], [1, 2, 3, 4, 5, 12], [6,7,14,15,16,17], [12,13,14,15,16,17]]  :: Jogos
 exResultado = [12,13,14,15,16,17] :: Resultado
 
 -- Checa cada um dos jogos. Quando coincide com o resultado, retorna 1 somado a checagem dos demais jogos
@@ -23,4 +23,20 @@ mitnick (r:rs) (j) | elem r j = (1 + mitnick rs j)
 acertos :: Resultado -> Jogos -> [Int]
 acertos r [] = [] -- Caso base é quando testarmos todos os jogos
 acertos [] (j:js) = [] -- Para o caso hipotético em que o resultado é vazio
-acertos r (j:js) = (mitnick r j):(acertos r js)  
+acertos r (j:js) = (mitnick r j):(acertos r js)
+
+turing :: [Int] -> Int -> Int
+turing [] _ = 0
+turing (a:as) q | a == q = (1 + (turing as q))
+                | otherwise = (turing as q)
+
+numPremios :: Resultado -> Jogos -> (Int, Int, Int)
+numPremios r j = ((turing (acertos r j) 4),(turing (acertos r j) 5),(turing (acertos r j) 6))
+
+
+
+
+
+
+
+
