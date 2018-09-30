4cb9c40fd7f10c9a75591ef80e70227bbe445360
gustavoisidio <gisf@cin.ufpe.br>
2018-09-30 20:01:43 -0300

    2018.1

---
+++ Prova_1_2018_1/q1.hs
@@ -0,0 +1,16 @@
+data Time = Australia | Dinamarca | Franca | Peru |
+            Argentina | Croacia | Islandia | Nigeria deriving (Show, Eq)
+
+type Jogo = (Time, Int, Time, Int)
+
+
+
+-- A)
+bakur :: Time -> Jogo -> Int
+bakur timeq (time1, g1, time2, g2) | timeq == time1 = g1
+                                   | timeq == time2 = g2
+                                   | otherwise = 0
+
+gols :: Time -> [Jogo] -> Int
+gols _ [] = 0
+gols timeq (x:xs) = (bakur timeq x) + (gols timeq xs)
