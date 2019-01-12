f024c39770ba47b9d604272f044b8056402ccef6
gustavoisidio <gisf@cin.ufpe.br>
2019-01-12 18:59:59 -0300

    Q3 okay

--- Prova_1_2018_2/Q3.hs
+++ Prova_1_2018_2/Q3.hs
@@ -1,24 +1,24 @@
 type Dicionario = [(Int, String)]
-
-
--- exemplo: 
-meuDicionario :: Diccionario
+meuDicionario :: Dicionario
 meuDicionario = [(1, "casa"), (3, "cafe"), (4, "teria"), (6, "era"), (7, "uma")]
-teste = "a 1 tinha 3 mas nao 6 7 34, a 1 6 7 sorve4" :: String
-decode meuDicionario teste ---> "a casa tinha cafe mas nao era uma cafeteria, a casa era uma sorveteria"
 
+teste = "a 1 tinha 3 mas nao 6 7 34, a 1 6 7 sorve4" :: String
+testeResposta = "a casa tinha cafe mas nao era uma cafeteria, a casa era uma sorveteria" :: String
+
+kut = ["0","1","2","3","4","5","6","7","8","9"] :: [String] -- map show [0..9]
 
 decode :: Dicionario -> String -> String
 decode _ [] = []
-decode 
+decode dic (d:ds) | elem (d:[]) kut = (searchDicionario (sti (d:[])) dic) ++ (decode dic ds)
+                  | otherwise = (d:[] ++ (decode dic ds))
 
-searchDicionario :: Int -> String
+searchDicionario :: Int -> Dicionario -> String
+searchDicionario x [] = show x
+searchDicionario x ((n, s):ns) | x == n = s
+                               | otherwise = searchDicionario x ns 
 
-
-
-
-
-
+sti :: String -> Int
+sti x = read x
 
 
 
