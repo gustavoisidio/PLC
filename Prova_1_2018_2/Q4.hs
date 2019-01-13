d5e61f40dccf401372cb9fa1d17a66c83974b468
gustavoisidio <gisf@cin.ufpe.br>
2019-01-12 21:33:32 -0300

    Questão 4 okay. Todas as questões concluídas.

--- Prova_1_2018_2/Q4.hs
+++ Prova_1_2018_2/Q4.hs
@@ -1,34 +1,30 @@
-import Data.String
+type DicionarioT = Tree Int String
+data Tree chave valor = Node chave valor (Tree chave valor) (Tree chave valor)
+                      | Leaf
 
-data DicionarioT t = Node Int String (DicionarioT t) (DicionarioT t)
-                   | Leaf t
-
-
--- data Tree t = Node t (Tree t) (Tree t) 
---             | Leaf t                   
-
-meuDicionarioT :: DicionarioT t
-meuDicionarioT = Node 4 "teria" (Node 3 "cafe" (Node 1 "casa" Leaf t Leaf t) Leaf t)
-                                (Node 6 "era" Leaf t (Node 7 "uma" Leaf t Leaf t))
+meuDicionarioT :: DicionarioT
+meuDicionarioT = Node 4 "teria" (Node 3 "cafe" (Node 1 "casa" Leaf Leaf) Leaf)
+                                (Node 6 "era" Leaf (Node 7 "uma" Leaf Leaf))
 
 teste = "a 1 tinha 3 mas nao 6 7 34, a 1 6 7 sorve4" :: String
 testeResposta = "a casa tinha cafe mas nao era uma cafeteria, a casa era uma sorveteria" :: String
 
 kut = ["0","1","2","3","4","5","6","7","8","9"] :: [String] -- map show [0..9]
 
--- decode :: DicionarioT -> String -> String
--- decode _ [] = []
--- decode dic (d:ds) | elem (d:[]) kut = (searchDicionario (sti (d:[])) dic) ++ (decode dic ds)
---                   | otherwise = (d:[] ++ (decode dic ds))
+decodeTree :: DicionarioT -> String -> String
+decodeTree _ [] = []
+decodeTree dic (d:ds) | elem (d:[]) kut = (searchDicionario (sti (d:[])) dic) ++ (decodeTree dic ds)
+                      | otherwise = (d:[] ++ (decodeTree dic ds))
 
--- sti :: String -> Int
--- sti x = read x
+sti :: String -> Int
+sti x = read x
 
--- -- So precisa ler direito a BST
--- searchDicionario :: Int -> DicionarioT -> String 
--- searchDicionario (Leaf) = []
--- searchDicionario val (Node root str rest) | (val < root) = "eh menor"
---                                           | otherwise = "nao eh menor"
+-- So precisa ler direito a BST
+searchDicionario :: Int -> DicionarioT -> String 
+searchDicionario _ (Leaf) = []
+searchDicionario val (Node root str tE tD) | (val < root) = searchDicionario val tE
+                                           | (val > root) = searchDicionario val tD
+                                           | otherwise = str
 
 
 
