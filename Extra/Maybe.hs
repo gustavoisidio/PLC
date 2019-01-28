9b3deff37182bc7508a2f7e365ae11f375f89fdc
gustavoisidio <gustavoisidio@Jarvis.local>
2019-01-28 19:49:06 -0300

    update

---
+++ Extra/Maybe.hs
@@ -0,0 +1,28 @@
+import Prelude hiding (Maybe (..))
+
+data Maybe a = Just a 
+             | Nothing
+               deriving (Show)
+
+
+imotep :: Maybe t -> t
+imotep (Just c) = c
+
+casac :: String -> String
+casac x = x
+
+-- data SimpleTree = Node "parent" (Node "left child" Empty Empty)
+--                                 (Node "right child" Empty Empty)
+-- Empty is like null
+
+safeSecond :: [a] -> Maybe a
+safeSecond [] = Nothing
+safeSecond xs = if null (tail xs)
+                then Nothing
+                else Just (head (tail xs))
+
+
+
+tidySecond :: [a] -> Maybe a
+tidySecond (_:x:_) = Just x
+tidySecond _ = Nothing
