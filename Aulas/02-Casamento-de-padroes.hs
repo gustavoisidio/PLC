a53474ae6014dc07d100597043a758d540c82abd
gustavoisidio <gisf@cin.ufpe.br>
2019-08-20 21:40:51 -0300

    Comments cleared

--- Aulas/02-Casamento-de-padroes.hs
+++ Aulas/02-Casamento-de-padroes.hs
@@ -1,4 +1,3 @@
--- Primeiramente, é importante que os padrões coincidam com o tipo da entrada. Se a entrada é inteira, por exemplo, "3.14" não é um padrão que funcionará.
 
 adivinheUmNumero :: Int -> String
 adivinheUmNumero 0 = "Aeeeee! 0!"
@@ -6,20 +5,12 @@ adivinheUmNumero 73 = "Aeeeee! 73!"
 adivinheUmNumero 5 = "Aeeeee! 5!"
 adivinheUmNumero 83274 = "Aeeeee! 83274"
 
--- Na função acima, um inteiro é recebido como entrada e comparado com cada um dos padrões, em ordem, de cima para baixo. Caso coincida com um deles, uma string é retornada e todos os padrões abaixo dele são ignorados, pondo um fim nas comparações.  
-
--- No entanto, caso os padrões da função não cubram todas as possibilidades de padrões, temos um erro. Nesse caso em particular, uma solução é usar "_" como um dos padrões, que funciona como um 'else' ou 'otherwise'.
-
 adivinheUmNumero2 :: Int -> String
 adivinheUmNumero2 0 = "Aeeeee! 0!"
 adivinheUmNumero2 73 = "Aeeeee! 73!"
 adivinheUmNumero2 5 = "Aeeeee! 5!"
 adivinheUmNumero2 83274 = "Aeeeee! 83274"
 adivinheUmNumero2 _ = "Xiiee, errou :'("   
-
--- É importante ter cuidado na utilização de "_" pois, como dito acima, a ordem dos padrões importa. Se for o primeiro dos padrões, por exemplo, a função vai cair nele independente da entrada. Na prática, é isso que ele significa "Independente da entrada, se chegou aqui, é isso". Executando a função abaixo para qualquer inteiro, a saída sempre será a mesma.
-
--- Em alguns casos, temos até um warning informando que aquelas linhas de código após o primeiro padrão serão "redundantes" o que de fato faz sentido, já que independente delas, acontecerá o mesmo. 
 
 adivinheUmNumero3 :: Int -> String
 adivinheUmNumero3 _ = "Xiiee, errou :'("   
--- Aulas/08-Compreensao-de-listas.hs
+++ Aulas/08-Compreensao-de-listas.hs
@@ -1,20 +1,4 @@
--- Em Haskell, a definição de S = { 2 * x | x e N, x ≦ 10} pode ser descrita de diversas formas. Uma delas, usa um conceito chamado de "compreensao de listas"
-
--- [x*2 | x <- [1..10]] retorna [2,4,6,8,10,12,14,16,18,20]
--- [x*2 | x <- [1..10], x*2 >= 12] retorna [12,14,16,18,20]
--- [ x | x <- [50..100], x `mod` 7 == 3] retorna [52,59,66,73,80,87,94]
-
--- Dentro dessa definição, podemos, inclusive, utilizar funções como a função abaixo:
 menorQue20 :: Int -> Bool
 menorQue20 x = x < 20
-
--- Podemos utilizá-la, por exemplo, com [x | x <- [19..100000], menorQue20 x] que retorna [19]
-
-
- 
-
-
-
-
 
 
