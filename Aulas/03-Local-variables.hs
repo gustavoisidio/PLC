6db5efa9d9cdce16bcda26591bb5d4da5ae0c8b4
gustavoisidio <gisf@cin.ufpe.br>
2019-08-20 21:40:51 -0300

    Comments cleared

--- Aulas/03-Local-variables.hs
+++ Aulas/03-Local-variables.hs
@@ -1,16 +1,6 @@
--- Haskell não possui a definição de variável. Nós não podemos, por exemplo, declarar uma variável global e alterá-la dentro de funções. Também não podemos declarar uma variável em um certo ponto da função e alterá-la ainda dentro do escopo dela.
-
--- O que podemos é declarar valores que podem ser usados no decorrer do código, mas são valores definidos em um único ponto. Valores estáticos, como a string abaixo:
-
 melhorLinguagem = "Haskell"
 
--- Se tentarmos verificar o tipo de melhorLinguagem, veremos que trata-se de uma String, ainda que não tenhamos dito isso a ela. No entanto, podemos também dizer qual o tipo dela, de forma semelhante a que usamos para definir os tipos de entrada e saída de uma função:
-
 melhorLinguagem :: String
-
--- Ainda sobre variáveis, o que podemos fazer é, no máximo, declarar "variáveis" locais que carregam consigo definições. 
-
--- Ao invés de um código cheio de cálculos e expressões gigantes cujo entendimento requer pausa e atenção, é preferível um código limpo, organizado com entendimento descomplicado. Um bom alvo, inclusive, é produzir código que seja tão fácil de ser lido quanto a própria linguagem nativa. Na tentativa de prover essa fluidez, surgem as variáveis locais. A expressão abaixo recebe dois argumentos e utiliza duas keywords: let, marcando o início da declaração das variáveis e in, iniciando o código propriamente dito. Em muitos casos, essas "variáveis locais" tem mais cara de função. Uma função que só pode ser acessada dentro do escopo da função na qual foi criada.
 
 lend amount balance = let reserve = 100
                           newBalance = balance - amount 
@@ -18,24 +8,17 @@ lend amount balance = let reserve = 100
                          then Nothing
                          else Just newBalance
 
--- Outra forma seria utilizando where, onde a declaração das variáveis é feita posteriormente, o que, na opinião de alguns, deixa o código mais flúido que a primeira. let inicia a definição e in indica em quais expressões utilizaremos essas definições. No caso de where, as expressões aparecem antes e as definições são feitas nele.
-
 lend2 amount balance = if amount < reserve * 0.5
                        then Just newBalance
                        else Nothing
         where reserve = 100
               newBalance = balance - amount
 
--- Aqui, temos outra aplicação de where criando algo que se assemelha a casamento de padrões.
-
 pluralise :: String -> [Int] -> [String]
 pluralise word counts = map plural counts
        where plural 0 = "no " ++ word ++ "s" -- Plural is a local function
              plural 1 = "one " ++ word
              plural n = show n ++ " " ++ word ++ "s"              
-
-
--- Já nos exemplos abaixo, vemos a mesma função feita de formas diferentes. Na primeira, utilizando let e in enquanto que, na segunda, é feito o uso de map e where com o auxilio de uma função lines que recebe uma String e separa o que precede '\n' do que o sucede, caso o encontre. 
 
 splitLines :: String -> [String]
 splitLines [] = []
--- Aulas/04-Tipos-e-conversoes.hs
+++ Aulas/04-Tipos-e-conversoes.hs
@@ -1,21 +1,10 @@
--- Cada linguagem tem restrições quanto a utilização de tipos. A linguagem C é um grande exemplo de "weakly typed". Conseguimos realizar operações com tipos diferentes simplesmente usando cast, conseguimos somar caracteres ( uma operação normalmente criada para números ), etc.
-
--- Haskell, por outro lado, é um exemplo de linguagem "strongly typed" e, na prática, isso é o oposto da permissividade de C. Nela, existem relações restritas entre tipos e regras fortes sobre como esses tipos devem se relacionar e se devem. Não é possível, por exemplo, sobar dois caracteres. A operação 'a' + 'b' é algo que não faz o menor sentido em Haskell.
-
--- Se quisessemos realmente somar dois caracteres, poderíamos fazê-lo com o auxílio da função fromEnum ( "antônimo" de toEnum ). Abaixo, a função sumCaracteres recebe dois caracteres, os converte e retorna a soma deles. 
-
 sumCaracteres :: Char -> Char -> Int
 sumCaracteres x y = ( fromEnum x ) + ( fromEnum y )
-
--- Nesse mesmo contexto de conversão de tipos, dentre as tantas coisas que fazemos em Haskell, uma que aparece com frequencia é a necessidade de converter inteiros em Strings e vice versa. Para a nossa sorte, Haskell já possui funções com esse propósito e elas são "show" e "read".
-
 intToString :: Int -> String
 intToString x = show x
 
 stringToInt :: String -> Int 
 stringToInt x = read x
-
--- Poderíamos, por exemplo, criar uma função que soma dois valores a partir de duas strings
 
 sumStrings :: String -> String -> String
 sumStrings x y = show xy
